53fe1d68662147ab9e4a02ce2e5ad43b53d4ad4e
dlobregon <troyanrex@gmail.com>
2024-10-14 20:37:31 -0600

    adding processionForm type

--- src/types/ProcesionType.tsx
+++ src/types/ProcesionType.tsx
@@ -1,3 +1,4 @@
+import { type Dayjs } from 'dayjs'
 export interface ProcesionType {
   procesion: number
   nombre: string
@@ -7,4 +8,17 @@ export interface ProcesionType {
   tipo_procesion: number
   sexo: number
   brazos: number
+  total_turnos: number
+}
+
+export interface ProcesionFormType {
+  procesion: number | undefined
+  nombre: string | undefined
+  fecha: string | undefined | Dayjs
+  comentario: string | undefined
+  habilitado: boolean | undefined
+  tipo_procesion: number | undefined
+  sexo: number | undefined
+  brazos: number | undefined
+  total_turnos: number | undefined
 }
